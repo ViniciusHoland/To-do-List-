52a555be0e218aebdd45e473a738ffff0406b943
ViniciusDev <viniciusdev10@gmail.com>
2025-02-06 09:56:13 -0300

    finished req login with database

--- routes/public/login.js
+++ routes/public/login.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express()
 
@@ -7,11 +8,28 @@ app.use(express.json())
 
 const router =  express.Router()
 
+const prisma = new PrismaClient()
+
 router.post('/login', async (req, res) => {
 
-   
+    try{
+        const {email, password} = req.body
 
-    res.status(201).json({message: 'Login sucessfully'})
+        const user = await prisma.user.findUnique({where: {email: email} })
+
+        if(!user){
+            return res.status(404).json({message: 'User not found'})
+        }
+        if(user.password === password){
+            res.status(201).json({message: 'Login sucessfully'})
+        }
+
+        return res.status(400).json({message: 'senha incorrect'})
+
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({message: 'Internal server error'})
+    }
 
 })
 

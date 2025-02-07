4659a3a6f53a8d1a973ce15ca9288054725d7f6e
ViniciusDev <viniciusdev10@gmail.com>
2025-02-07 11:12:01 -0300

    created user relationship and list to add to the database

--- routes/private/toDoList.js
+++ routes/private/toDoList.js
@@ -21,21 +21,43 @@ router.post('/todolist', async (req, res) => {
         const [dia, mes , ano] = date.split('/')
         const dateFormat =  `${ano}-${mes}-${dia}`
         const newDate = new Date(dateFormat)
+
+        const auth =  req.headers.authorization
+        const token = auth.split(' ')[1]
+
+        if(!token){
+            return res.status(401).json({message: 'Token not provided or invalid'})
+        }
+
+        const JWT_SECRET = process.env.JWT_SECRET
+
+        const decodedToken = jwt.verify(token, JWT_SECRET)
         
+        const user = await prisma.user.findUnique({where: { id : decodedToken.id}})
+
+        if(!user){
+            return res.status(401).json({message: 'User not found'})
+        }
+
+        const userId = user.id
 
         const newToDoList = await prisma.list.create({
             data: {
                 title,
                 description,
-                date: newDate
+                date: newDate,
+                user:  {
+                    connect: {id: userId}
+                } 
             }
+    
         })
 
         return res.status(201).json({message: "created to do list successfully"})
 
     }catch(error){
         console.error(error)
-        res.status(500).json({ error: 'Internal Server Error' })
+        res.status(500).json({ error: 'Internal Server Error for created investment' })
     }
 
 })
@@ -48,7 +70,7 @@ router.get('/todolist', async (req,res) => {
         const token = auth.split(' ')[1]
 
         if(!token){
-            return res.status(401).json({message: 'Token not provided'})
+            return res.status(401).json({message: 'Token not provided or invalid'})
         }
 
         const JWT_SECRET = process.env.JWT_SECRET
